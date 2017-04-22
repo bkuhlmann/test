bdd3f34445ea7ba6b6c1288758138010434cf0ce
Brooke Kuhlmann <brooke@alchemists.io>
2017-04-22 08:01:02 -0600

    Added project skeleton

---
+++ spec/spec_helper.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "simplecov"
+
+SimpleCov.start :strict unless ENV["COVERAGE"] == "no"
+Bundler.require :tools
+
+require "test"
+
+SPEC_ROOT = Pathname(__dir__).realpath.freeze
+
+using Refinements::Pathname
+
+Pathname.require_tree SPEC_ROOT.join("support/shared_contexts")
+
+RSpec.configure do |config|
+  config.color = true
+  config.disable_monkey_patching!
+  config.example_status_persistence_file_path = "./tmp/rspec-examples.txt"
+  config.filter_run_when_matching :focus
+  config.formatter = ENV.fetch("CI", false) == "true" ? :progress : :documentation
+  config.order = :random
+  config.pending_failure_output = :no_backtrace
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.warnings = true
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+    mocks.verify_partial_doubles = true
+  end
+
+  Kernel.srand config.seed
+end
